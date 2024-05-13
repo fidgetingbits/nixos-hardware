e045f93ab2a46f46934ef9b4ff12674196393a90
fidgetingbits <fidgetingbits@users.noreply.github.com>
2024-05-13 09:42:55 +0800

    zenbook stuff

--- asus/zenbook/s13/default.nix
+++ asus/zenbook/s13/default.nix
@@ -6,14 +6,21 @@
   ...
 }: {
   imports = [
-    ../../../common/cpu/intel/tiger-lake
+    # FIXME: Add tiger lake folder
+    # FIXME: Add iris xe graphics test
+    ../../../common/cpu/intel
     ../../../common/pc/laptop
     ../../../common/pc/laptop/ssd
   ];
   hardware.enableRedistributableFirmware = lib.mkDefault true;
 
+  boot.initrd.kernelModules = ["xe"];
   boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod"];
-  boot.kernelModules = ["kvm-intel"];
+  boot.kernelModules = ["kvm-intel" "xe"];
+
+  # 6.8 and newer kernels have new Iris XE graphics support
+  boot.kernelParams = [ "i915.force_probe=!9a49" "xe.force_probe=9a49" ]
+
 
   services.thermald.enable = lib.mkDefault true;
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
